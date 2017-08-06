78184bc21243316b8d859c7eabb7992b52c7da05
sirius89 <avet.sargsyan@gmail.com>
2017-08-06 09:41:46 +0400

    lightCtrl

--- api/controllers/lights/lightCtrl.js
+++ api/controllers/lights/lightCtrl.js
@@ -6,12 +6,12 @@ let broker = require('../../broker/mosca');
 exports.action = (req, res) => {
     let message = {
         topic: 'Light/sub',
-        payload: req.params.action, // or a Buffer
+        payload: req.params.number, // or a Buffer
         qos: 0, // 0, 1, or 2
         retain: false // or true
     };
     broker.publish(message, function() {
-        console.log('Light/sub -> '+req.params.action+' sent');
+        console.log('Light/sub -> '+req.params.number+' sent');
         res.send('OK');
     });
 };
